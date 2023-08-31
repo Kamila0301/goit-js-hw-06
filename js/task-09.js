32d625e1082c63d8435ee24dd7a93eeef931e007
Kamila0301 <bogda0301@gmail.com>
2023-08-31 21:29:21 +0300

    update

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,12 @@
+const colorEl = getRandomHexColor();
+bodyEl.style.backgroundColor = colorEl;
+spanText.textContent = colorEl;
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
