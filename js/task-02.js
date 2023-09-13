bd1bf10d964e3b08c1782720713d6316fef949bf
Kamila0301 <bogda0301@gmail.com>
2023-09-13 13:49:10 +0300

    update

--- js/task-02.js
+++ js/task-02.js
@@ -8,11 +8,15 @@ const ingredients = [
 ];
 
 const ulIngredients = document.getElementById("ingredients");
+
+let newArray = [];
+
 ingredients.forEach((ingredient) => {
   const li = document.createElement("li");
   li.textContent = ingredient;
   li.classList.add("item");
 
+  newArray.push(li);
 });
- 
- ulIngredients.append([...ingredients]);
+
+ulIngredients.append([...newArray]);
